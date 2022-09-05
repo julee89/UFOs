d33e9c6708483657418e32615ed1de63b939cbd8
Jung Lee <Junlee@gap.com>
2022-09-04 21:52:59 -0700

    adding final view of table and readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # UFOs
+
+## Overview
+The purpose of this analysis is to help Dana build a webpage with dynamic tables which will allow the user to filter the webpage for an in depth view of UFO viewings by city, state, country, and shape.
+
+## Results
+The initial webpage is a compiled view of all the UFO viewings for all the states, cities, countries and shapes. However, with the dynamic filtered buttons the user can view only information that they would like to view.
+
+[Unfiltered View](Resources/ufo.list.PNG)
+
+On the webpage the user will first scroll down to the filters and tables. This table will hold all the data on the table as an unfiltered list as shown here. Thist list has a defaulted date value of 1/10/2010.
+
+Then going through the filter boxes, we can see the first option to fill out on the filter is "Enter a City" where the user will provide a city in lowercase. In this example the user will input "el cajon" in the provided textbox.
+
+[El Cajon Filter](Resources/ufo.elcajon.PNG)
+
+Then the next filter shown will be the
+"Enter a State" and "Enter a Country" where the user will provide a state value and country value in lowercase. In this example the user will input "ca" in the provided state textbox and "us" in the country textbox.
+
+[El Cajon & CA Filter](Resources/ufo.elcajonca.PNG)
+
+Then the next filter shown will be
+"Enter a Shape" where the user will provide a shape they would like to view shown in the previously filtered, city, state and country. In this example the user will input "triangle" in the provided textbox.
+
+[El Cajon, CA US Triangle Shape Filter](Resources/ufo.elcajoncatrianglefilter.PNG)
+
+Once the following values are provided in the text boxes, it will generate a view such as a the follow shown here.
+
+[El Cajon, CA US Triangle Shape View](Resources/ufo.elcajoncatriangle.PNG)
+
+Following the flow of the columns, we can see that they should all alien sightings in El Cajon, CA in the US with the triangle shapes - there are two rows in the table.
+
+## Summary
+One major drawback of this new filtered design is that it is case-sensitive. To make this table more dynamic, it could be beneficial to make it so it is not case-sensitive and allow for the table to increase or decrease columns based on the users needs.
--- static/js/app.js
+++ static/js/app.js
@@ -1,61 +1,84 @@
-// import the data from app.js
-
+// from data.js
 const tableData = data;
 
-// Reference the HTML table using d3
-
-var tbody = d3.select("tbody")
-
-// function to build table with data
+// get table references
+var tbody = d3.select("tbody");
 
 function buildTable(data) {
-    tbody.html("");
+  // First, clear out any existing data
+  tbody.html("");
 
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
 
-// forEach loop to iterate through each row of data
-    data.forEach((dataRow) => {
-        
-        // Find the tbody tag in the html and add a table row (tr)
-        let row = tbody.append("tr");
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+    });
+  });
+}
 
-        // Loop through each dataRow argument referencing one object from the array
-        // and append one object to a row on the table.
-        Object.values(dataRow).forEach((val) => {
+// 1. Create a variable to keep track of all the filters as an object.
+var filters = {};
 
-            // creating variable to append the value to the table data (td)
-            let cell = row.append("td");
+// 3. Use this function to update the filters. 
+function updateFilters() {
 
-            // add the values
-            cell.text(val);
-    }
-   
-    );
+    // 4a. Save the element that was changed as a variable.
 
-});
+    let changedElement = d3.select(this);
 
-};
+    // 4b. Save the value that was changed as a variable.
+    
+    let elementValue = changedElement.property("value");
+    console.log(elementValue)
 
-function handleClick() {
-    // Grab the datetime value from the filter
-    let date = d3.select("#datetime").property("value");
-    let filteredData = tableData;
+    // 4c. Save the id of the filter that was changed as a variable.
+    let filteredId = changedElement.attr("id")
+    console.log(filteredId);
+
   
-     // Check to see if a date was entered and filter the
-    // data using that date.
-    if (date) {
-      // Apply `filter` to the table data to only keep the
-      // rows where the `datetime` value matches the filter value
-      filteredData = filteredData.filter(row => row.datetime === date);
+    // 5. If a filter value was entered then add that filterId and value
+    // to the filters list. Otherwise, clear that filter from the filters object.
+
+    if (elementValue) {
+      filters[filteredId] = elementValue;
     }
+
+    else {
+      delete filters[filteredId];
+    }
+ 
   
-     // Rebuild the table using the filtered data
-    // @NOTE: If no date was entered, then filteredData will
-    // just be the original tableData.
-    buildTable(filteredData);
+    // 6. Call function to apply all filters and rebuild the table
+    filterTable();
+  
   }
   
-  // Attach an event to listen for the form button
-  d3.selectAll("#filter-btn").on("click", handleClick);
+  // 7. Use this function to filter the table when data is entered.
+  function filterTable() {
+  
+    // 8. Set the filtered data to the tableData.
+    let filteredData = tableData;
+
+    // 9. Loop through all of the filters and keep any data that
+    // matches the filter values
+    Object.entries(filters).forEach(([dataKey, inputValue]) => {
+      filteredData = filteredData.filter(row => row[dataKey] === inputValue);
+    });
+
+    // 10. Finally, rebuild the table using the filtered data
+    buildTable(filteredData)
+  }
+  
+  // 2. Attach an event to listen for changes to each filter
+  d3.selectAll("input").on("change", updateFilters);
+  
   
   // Build the table when the page loads
   buildTable(tableData);
